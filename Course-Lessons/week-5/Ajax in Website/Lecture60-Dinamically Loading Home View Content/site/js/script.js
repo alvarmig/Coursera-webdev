a30cfcd9ae906459309ce0528ca685235909dab3
alvarmig <43011534+alvarmig@users.noreply.github.com>
2019-05-28 20:50:10 -0500

    added lecture60

--- Course-Lessons/week-5/Ajax in Website/Lecture60-Dinamically Loading Home View Content/site/js/script.js
+++ Course-Lessons/week-5/Ajax in Website/Lecture60-Dinamically Loading Home View Content/site/js/script.js
@@ -19,38 +19,40 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   });
 });
 
-(function (global) { 
-  var dc = {};
+(function (global) {
 
-  var homeHtml = "snippets/home-snippet.html";
+var dc = {};
 
-  //Convinience function for inserting innerHTML for 'select'
-  var insertHtml = function (selector, html) {
-    var targetElem = document.querySelector(selector);
-    targetElem.innerHTML = html;
-  };
-  // Show loading icon inside element identified by 'selector'.
-  var showLoading = function(selector) {
-    var html = "<div class='text-center'>";
-    html += "<img src='images/ajax-loader.gif'></div>";
-    insertHtml(selector, html);
-  };
+var homeHtml = "snippets/home-snippet.html";
 
-  // On page load (before images or CSS)
-  document.addEventListener("DOMContentLoaded", function (event) { 
+// Convenience function for inserting innerHTML for 'select'
+var insertHtml = function (selector, html) {
+  var targetElem = document.querySelector(selector);
+  targetElem.innerHTML = html;
+};
 
-    //On first load, show home view
-    showLoading("#main-content");
-    $ajaxUtils.sendGetRequest(
-      homeHtml, 
-      function (responseText) {
-        document.querySelector("main-content")
-          .innerHTML = responseText;
-    },
-    false);
-  });
+// Show loading icon inside element identified by 'selector'.
+var showLoading = function (selector) {
+  var html = "<div class='text-center'>";
+  html += "<img src='images/ajax-loader.gif'></div>";
+  insertHtml(selector, html);
+};
 
-  global.$dc = dc;
+// On page load (before images or CSS)
+document.addEventListener("DOMContentLoaded", function (event) {
 
+// On first load, show home view
+showLoading("#main-content");
+$ajaxUtils.sendGetRequest(
+  homeHtml,
+  function (responseText) {
+    document.querySelector("#main-content")
+      .innerHTML = responseText;
+  },
+  false);
+});
+
+
+global.$dc = dc;
 
 })(window);
