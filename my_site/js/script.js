361fb522306ba59dd5b8124148bc52a9a8bd89ed
alvarmig <43011534+alvarmig@users.noreply.github.com>
2019-06-01 21:29:49 -0500

    assets for carousel

---
+++ my_site/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true,
+        margin:10,
+        nav:true,
+        merge:true,
+        responsive:{ 
+            0:{ items:1 }, 
+            767:{ items:2 }, 
+            991:{ items:3 },
+            1200:{items:4 } 
+        } 
+    })
+});
