a87726a67d9283e93ef6acb40a1be7f7a6fae3cd
alvarmig <43011534+alvarmig@users.noreply.github.com>
2019-06-03 19:41:13 -0500

    additions to the mysite project

--- my_site/js/script.js
+++ my_site/js/script.js
@@ -1,6 +1,33 @@
 /** https://owlcarousel2.github.io/OwlCarousel2/ **/
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+
+  $(window).scroll(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+
+  $('.owl-carousel').owlCarousel({
         loop:true,
         autoplay:true,
         autoplayTimeout:4000,
@@ -14,5 +41,5 @@ $(document).ready(function(){
             991:{ items:3 },
             1200:{items:4 } 
         } 
-    })
+    });
 });
